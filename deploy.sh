1ddfcf0cdd1d58c1c1ce3c253e0758a8936f915a
heartfulmoon <tpecosin@gmail.com>
2020-05-08 11:55:38 +0900

    rebuilding site 2020年  5月  8日 金曜日 11:55:32 JST

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 #hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-#hugo  --theme=beautifulehugo
-hugo  -P
+hugo  --theme=hugo-theme-iris
 
 # Go To Public folder
 cd public
